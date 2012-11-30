1351fbe8f53d3418e6c175ce3a633a80c7b2125b
Astro <astro@spaceboyz.net>
2012-12-01 00:02:33 +0100

    this works just fine

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 var contexts; /* input, play, log jQuery */
 
-var currentLine = 0;
+var scrollInterval = 25;
+var currentLine = 0, ty = 0, y = 0;
 var lines = [];
-var playing = true;
+var playing = false;
 
 function setCurrentLine(line) {
-    if (playing) {
+    $('#lines').find('.line').eq(currentLine).removeClass('current');
+    currentLine = line;
+    $('#lines').find('.line').eq(currentLine).addClass('current');
+
+    var wh = $(window).innerHeight();
+    ty = -wh + 64;
+    for(var i = 0; i <= currentLine; i++) {
+	var h = $('#lines').find('.line').eq(i).outerHeight();
+	if (i < currentLine)
+	    ty += h;
+	else if (h >= wh)
+	    ty += h;
+	else
+	    ty += wh - 68;
     }
+    updateScrolling();
+}
+
+var scrollTimeout;
+
+function updateScrolling() {
+    function setScrollTimeout() {
+	if (!scrollTimeout)
+	    scrollTimeout = setTimeout(function() {
+					   scrollTimeout = null;
+					   updateScrolling();
+				       }, scrollInterval);
+    }
+
+    var scrollSpeed = (playing ? 4 : 40) * (0.5 + Math.abs(y - ty) / $(window).innerHeight());
+
+    if (y < ty) {
+	y += scrollSpeed;
+	if (y > ty)
+	    y = ty;
+	setScrollTimeout();
+    } else if (y > ty) {
+	y -= scrollSpeed;
+	if (y < ty)
+	    y = ty;
+	setScrollTimeout();
+    } else {
+	y = ty;
+    }
+    // console.log("scrollSpeed", scrollSpeed, "y", y);
+    $('#lines').css('top', -y + "px");
 }
 
 var contextInit = {
@@ -16,6 +61,11 @@ var contextInit = {
     play: function() {
 	$('#play_prev').click(function() {
 	    setCurrentLine(Math.max(0, currentLine - 1));
+	});
+	$('#play_play').click(function() {
+	    playing = !playing;
+	    $('#play_play').text(playing ? "■" : "▶");
+	    updateScrolling();
 	});
 	$('#play_next').click(function() {
 	    setCurrentLine(Math.min(lines.length - 1, currentLine + 1));
@@ -32,14 +82,17 @@ var contextActivate = {
 
     play: function() {
 	var textarea = contexts.input.find('textarea');
-	lines = textarea.val().split(/\n/);
-	var el = contexts.play.find('.lines');
+	lines = textarea.val().split(/\s*\n\s*/);
+	var el = contexts.play.find('#lines');
 	el.empty();
 	lines.forEach(function(line) {
 	    var p = $('<p class="line"></p>');
 	    p.text(line);
 	    el.append(p);
 	});
+
+	y = - $(window).innerHeight() / 2;
+	setCurrentLine(0);
     },
 
     log: function() {
